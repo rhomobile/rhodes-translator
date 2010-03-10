6225c0e7f359ef0e83cc8eb03b7f87f7a1679e3a
Brian Moore <brian@Brian-Moores-MacBook-Pro.local>
2010-03-09 16:30:35 -0800

    Add more templates, application overridable templates

--- lib/rhodes_translator.rb
+++ lib/rhodes_translator.rb
@@ -8,10 +8,16 @@ module RhodesTranslator
   def load_erb(doc_def,action)
     @action = action
     @doc_def = doc_def
-        file = File.join(File.dirname(File.join(__rhoGetCurrentDir(), __FILE__)),
-                         'rhodes_translator',
+
+    file = File.join(Rho::RhoFSConnector.get_app_path('app'),
                          'templates',
                          "#{doc_def['type']}_erb.iseq")
+
+    
+    file = File.join(File.dirname(File.join(__rhoGetCurrentDir(), __FILE__)),
+                         'rhodes_translator',
+                         'templates',
+                         "#{doc_def['type']}_erb.iseq") unless File.exist? file
 
     eval_compiled_file(file, binding )
   end
