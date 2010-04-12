af005848a435cf49b08e18425304bcad7facce8f
Brian Moore <brian@Brian-Moores-MacBook-Pro.local>
2010-04-11 23:14:03 -0700

    Chnages from Anthony.

--- lib/rhodes_translator/validation.rb
+++ lib/rhodes_translator/validation.rb
@@ -11,23 +11,23 @@ module RhodesTranslator
     def validate(metadata, params)
       @errors = nil
 
-      action = metadata
-
-      if action.nil?
-        @errors = ["Unable to determine action"]
-        return @errors
-      end
-
-      metadata = metadata[action] unless metadata.nil?
+#      action = metadata
+#
+#      if action.nil?
+#        @errors = ["Unable to determine action"]
+#        return @errors
+#      end
+#
+#      metadata = metadata[action] unless metadata.nil?
       if metadata.nil?
-        @errors = ["No metadata found for action #{action}"]
+        @errors = ["No metadata found"]
         return @errors
       end
 
       @errors = []
 
       self.validate_data(metadata,params)
-  
+
       @errors
     end
 
@@ -52,39 +52,39 @@ module RhodesTranslator
 
         unless metadata['validation']['regexp'].nil?
           unless value =~ Regexp.new(metadata['validation']['regexp'])
-            @errors << "Field #{metadata['name']} did not pass regexp validation"
+            @errors << "#{metadata['label']} did not pass regexp validation"
           end
         end
 
         unless metadata['validation']['validators'].nil?
           metadata['validation']['validators'].each do |validator|
             unless value =~ Regexp.new(RhodesTranslator::Validation::VALIDATORS[validator])
-              @errors << "Field #{metadata['name']} did not pass validation #{validator}"
+              @errors << "#{metadata['label']} did not pass validation #{validator}"
             end
           end
         end
 
         unless metadata['validation']['min_len'].nil?
           if value.length < metadata['validation']['min_len'].to_i
-            @errors << "Field #{metadata['name']} too short"
+            @errors << "#{metadata['label']} too short"
           end
         end
 
         unless metadata['validation']['max_len'].nil?
           if value.length > metadata['validation']['max_len'].to_i
-            @errors << "Field #{metadata['name']} too short"
+            @errors << "#{metadata['label']} too short"
           end
         end
 
         unless metadata['validation']['min_value'].nil?
           if value.to_i < metadata['validation']['min_value'].to_i
-            @errors << "Field #{metadata['name']} too small"
+            @errors << "#{metadata['label']} too small"
           end
         end
 
         unless metadata['validation']['max_value'].nil?
           if value.to_i > metadata['validation']['max_value'].to_i
-            @errors << "Field #{metadata['name']} too large"
+            @errors << "#{metadata['label']} too large"
           end
         end
 
