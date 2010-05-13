1d35e2389e5644cf7b949cc3f8059e562d6cc605
Brian Moore <brian@Brian-Moores-MacBook-Pro.local>
2010-05-13 11:31:45 -0700

    fix validator specs

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -8,133 +8,132 @@ describe "Validation" do
 
   before(:each) do
     #@v = RhodesTranslator::Validation.new
-    @address = { 'label' => 'Address',
+    @meta = { 'label' => 'Address',
                  'value' => '123 fake st',
                  'name' => 'address1',
                  'validation' => {
                    }
                }
-    @meta = {'index' => @address}
-
+  
   end
   
   it "should validate using custom regexp" do
 
-    @meta['index']['validation']['regexp'] = '123 fake st'
+    @meta['validation']['regexp'] = '123 fake st'
 
-    data = {'address1' => '123 fake st', 'metadata_action' => 'index'}
+    data = {'address1' => '123 fake st'}
 
 
-    validate(@meta,data).length.should == 0
+    validate(@meta,data).should == nil
 
-    data = {'address1' => 'bad data', 'metadata_action' => 'index'}
+    data = {'address1' => 'bad data'}
 
     validate(@meta,data).length.should == 1
   end
 
   it "should have required validator" do
 
-    data = {'address1' => '', 'metadata_action' => 'index'}
-    @meta['index']['validation']['validators'] = ['required']
+    data = {'address1' => ''}
+    @meta['validation']['validators'] = ['required']
 
     validate(@meta,data).length.should == 1
 
-    data = {'address1' => 'required', 'metadata_action' => 'index'}
+    data = {'address1' => 'required'}
 
-    validate(@meta,data).length.should == 0
+    validate(@meta,data).should == nil
   end
 
   it "should have number validator" do
-    @meta['index']['validation']['validators'] = ['number']
-    data = {'address1' => '1234', 'metadata_action' => 'index'}
+    @meta['validation']['validators'] = ['number']
+    data = {'address1' => '1234'}
 
-    validate(@meta,data).length.should == 0
+    validate(@meta,data).should == nil
 
-    data = {'address1' => 'not a number', 'metadata_action' => 'index'}
+    data = {'address1' => 'not a number'}
 
     validate(@meta,data).length.should == 1
 
   end
 
   it "should have currency validator" do
-    @meta['index']['validation']['validators'] = ['currency']
-    data = {'address1' => '10.00', 'metadata_action' => 'index'}
+    @meta['validation']['validators'] = ['currency']
+    data = {'address1' => '10.00'}
 
-    validate(@meta,data).length.should == 0
+    validate(@meta,data).should == nil
 
-    data = {'address1' => '123', 'metadata_action' => 'index'}
+    data = {'address1' => '123'}
 
     validate(@meta,data).length.should == 1
 
   end
 
   it "should have email validator" do
-    @meta['index']['validation']['validators'] = ['email']
-    data = {'address1' => 'blah@blah.com', 'metadata_action' => 'index'}
+    @meta['validation']['validators'] = ['email']
+    data = {'address1' => 'blah@blah.com'}
 
-    validate(@meta,data).length.should == 0
+    validate(@meta,data).should == nil
 
-    data = {'address1' => 'not an email', 'metadata_action' => 'index'}
+    data = {'address1' => 'not an email'}
 
     validate(@meta,data).length.should == 1
 
   end
 
   it "should have phone validator" do
-    @meta['index']['validation']['validators'] = ['phone']
-    data = {'address1' => '408-555-1212', 'metadata_action' => 'index'}
+    @meta['validation']['validators'] = ['phone']
+    data = {'address1' => '408-555-1212'}
 
-    validate(@meta,data).length.should == 0
+    validate(@meta,data).should == nil
 
-    data = {'address1' => 'not an phone', 'metadata_action' => 'index'}
+    data = {'address1' => 'not an phone'}
 
     validate(@meta,data).length.should == 1
 
   end
 
   it "should have min_len validator" do
-    @meta['index']['validation']['min_len'] = 10
-    data = {'address1' => '1234567890', 'metadata_action' => 'index'}
+    @meta['validation']['min_len'] = 10
+    data = {'address1' => '1234567890'}
 
-    validate(@meta,data).length.should == 0
+    validate(@meta,data).should == nil
 
-    data = {'address1' => '12345', 'metadata_action' => 'index'}
+    data = {'address1' => '12345'}
 
     validate(@meta,data).length.should == 1
 
   end
 
   it "should have max_len validator" do
-    @meta['index']['validation']['max_len'] = 10
-    data = {'address1' => '12345', 'metadata_action' => 'index'}
+    @meta['validation']['max_len'] = 10
+    data = {'address1' => '12345'}
 
-    validate(@meta,data).length.should == 0
+    validate(@meta,data).should == nil
 
-    data = {'address1' => '12345678901', 'metadata_action' => 'index'}
+    data = {'address1' => '12345678901'}
 
     validate(@meta,data).length.should == 1
 
   end
 
   it "should have min_value validator" do
-    @meta['index']['validation']['min_value'] = 10
-    data = {'address1' => '12', 'metadata_action' => 'index'}
+    @meta['validation']['min_value'] = 10
+    data = {'address1' => '12'}
 
-    validate(@meta,data).length.should == 0
+    validate(@meta,data).should == nil
 
-    data = {'address1' => '2', 'metadata_action' => 'index'}
+    data = {'address1' => '2'}
 
     validate(@meta,data).length.should == 1
 
   end
 
   it "should have max_value validator" do
-    @meta['index']['validation']['max_value'] = 10
-    data = {'address1' => '9', 'metadata_action' => 'index'}
+    @meta['validation']['max_value'] = 10
+    data = {'address1' => '9'}
 
-    validate(@meta,data).length.should == 0
+    validate(@meta,data).should == nil
 
-    data = {'address1' => '11', 'metadata_action' => 'index'}
+    data = {'address1' => '11'}
 
     validate(@meta,data).length.should == 1
 
@@ -167,45 +166,44 @@ describe "Validation" do
                    }
                }
 
-    @view = { 'children' => [@address1, @address2, @address3] }
-    @meta = {'index' => @view}
+    @meta = { 'children' => [@address1, @address2, @address3] }
 
-    data = {'address1' => '11', 'address2' => '12', 'address3' =>'17', 'metadata_action' => 'index'}
-    validate(@meta,data).length.should == 0
+    data = {'address1' => '11', 'address2' => '12', 'address3' =>'17'}
+    validate(@meta,data).should == nil
 
-    data = {'address1' => '', 'address2' => '12', 'address3' =>'17', 'metadata_action' => 'index'}
+    data = {'address1' => '', 'address2' => '12', 'address3' =>'17'}
     validate(@meta,data).length.should == 1
 
-    data = {'address1' => '', 'address2' => '9', 'address3' =>'17', 'metadata_action' => 'index'}
+    data = {'address1' => '', 'address2' => '9', 'address3' =>'17'}
     validate(@meta,data).length.should == 2
 
-    data = {'address1' => '', 'address2' => '9', 'address3' =>'22', 'metadata_action' => 'index'}
+    data = {'address1' => '', 'address2' => '9', 'address3' =>'22'}
     validate(@meta,data).length.should == 3
 
   end
 
   it "should have an error for nil metadata" do
-    data = {'address1' => '12', 'metadata_action' => 'index'}
+    data = {'address1' => '12'}
 
-    validate(nil,data)[0].should == "No metadata found for action index"
+    validate(nil,data)[0].should == "No metadata found"
   end
 
   it "should have an error for validation with no name" do
-    @address1 = { 'label' => 'Address',
+    @meta = { 'label' => 'Address',
       'validation' => {
         'validators' => ['required']
       }
     }
 
-    @meta = {'index' => @address1}
-    data = {'address1' => '12', 'metadata_action' => 'index'}
+    
+    data = {'address1' => '12'}
 
     validate(@meta,data)[0].should == "Metadata element has validation with no name"
 
   end
 
   it "should have an error name with no value" do
-    data = {'address111' => '12', 'metadata_action' => 'index'}
+    data = {'address111' => '12'}
 
     validate(@meta,data)[0].should == "No value submitted for metadata element with name address1"
 
