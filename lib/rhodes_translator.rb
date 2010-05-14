60b4d3cca9ebe4cd3081049fea444e2586cada65
Brian Moore <brian@Brian-Moores-MacBook-Pro.local>
2010-05-13 17:53:12 -0700

    support being an extension for rhodes

--- lib/rhodes_translator.rb
+++ lib/rhodes_translator.rb
@@ -2,3 +2,5 @@ require 'rhodes_translator/translator'
 require 'rhodes_translator/binding'
 require 'rhodes_translator/validation'
 
+$rhodes_extensions ||= []
+$rhodes_extensions << File.dirname(__FILE__)
